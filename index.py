0df4474b5025d03954efa07afb1b8ff2d077966a
JMarianaStark <marianareyes9@gmail.com>
2020-06-04 19:18:37 -0600

    Se hicieron modificaciones en los botones

--- index.py
+++ index.py
@@ -32,18 +32,18 @@ txtmes=Entry(root,textvariable=entrada).place(x=135,y=130)
 
 lblaño=Label(text="Año",font=("Arial",11)).place(x=80,y=150)
 entrada=StringVar()
-entrada.set("")
+entrada.set("") 
 txtaño=Entry(root,textvariable=entrada).place(x=135,y=160)
 
 
-btnFuncion1 = Button(root, text= "Función  1",font=("Arial",10),width=10).place(x=20,y=190)
+btnFuncion1 = Button(root, text= "Función 1",font=("Arial",10),width=10).place(x=20,y=190)
 btnFuncion1 = Button(root, text= "Función 2",font=("Arial",10),width=10).place(x=115 ,y=190)
 btnFuncion1 = Button(root, text= "Función 3",font=("Arial",10),width=10).place(x=211,y=190)
 btnFuncion1 = Button(root, text= "Función 4",font=("Arial",10),width=10).place(x=305,y=190)
 btnFuncion1 = Button(root, text= "Función 5",font=("Arial",10),width=10).place(x=400,y=190)
 
 
-
+ 
 
 
 
